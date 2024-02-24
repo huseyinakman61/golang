97e32c7ff8ae2e3e8f57094a910160c3e42ed3a6
huseyinakman61 <huseyin.akman@guclunet.com>
2024-02-24 19:51:30 +0300

    vairables

---
+++ variables/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var name string
+	var age int
+	var isMarried bool
+	var price float32
+
+	name = "Hüseyin Akman"
+	age = 36
+	isMarried = true
+	price = 1.250
+
+	fmt.Println("Name:", name)
+	fmt.Printf("%T\n", name)
+	fmt.Println("Name:", reflect.TypeOf(name))
+	fmt.Println("Age:", age)
+	fmt.Printf("%T\n", age)
+	fmt.Println("Age:", reflect.TypeOf(age))
+	fmt.Printf("%T\n", isMarried)
+	fmt.Println("Married:", reflect.TypeOf(isMarried))
+
+	fmt.Println("price:", price)
+}
