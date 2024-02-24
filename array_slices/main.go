cba579c4fe555d9d07bc86a2fc3077a59941924d
huseyinakman61 <huseyin.akman@guclunet.com>
2024-02-24 20:12:00 +0300

    array + slices

---
+++ array_slices/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var country [3]string
+	country[0] = "Turkey"
+	country[1] = "Germany"
+	country[2] = "USA"
+
+	var colors = []string{"Blue", "Red", "Green", "Yellow", "Black", "White"}
+
+	fmt.Println("country 1:", country[0])
+	fmt.Println("country 2:", country[1])
+	fmt.Println("country 2:", country[2])
+	fmt.Println("country 2:", colors)
+}
