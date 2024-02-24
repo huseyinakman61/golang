508fa6bdd1977090a2287d44bad710c0aed16bdc
huseyinakman61 <huseyin.akman@guclunet.com>
2024-02-24 20:16:45 +0300

    slices

--- array_slices/main.go
+++ array_slices/main.go
@@ -4,15 +4,21 @@ import "fmt"
 
 func main() {
 
+	//array
 	var country [3]string
 	country[0] = "Turkey"
 	country[1] = "Germany"
 	country[2] = "USA"
 
-	var colors = []string{"Blue", "Red", "Green", "Yellow", "Black", "White"}
+	var colors = [6]string{"Blue", "Red", "Green", "Yellow", "Black", "White"}
+
+	//slices
+	var days = []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
 
 	fmt.Println("country 1:", country[0])
 	fmt.Println("country 2:", country[1])
 	fmt.Println("country 2:", country[2])
 	fmt.Println("country 2:", colors)
+	fmt.Println("country 2:", colors[3:5])
+	fmt.Println("country 2:", days)
 }
